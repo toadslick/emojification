f39288159371c02e46cef9c46280719e9adc8fc7
James Childress <jchildress@cvent.com>
2018-12-29 08:27:18 -0800

    tasks can be cancelled

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ const sampleCountInput  = document.getElementById('input-sample-count');
 const imageFileInput    = document.getElementById('input-image-file');
 const outputContainer   = document.getElementById('output-container');
 const progressContainer = document.getElementById('progress-bar-container');
+const cancelButton      = document.getElementById('cancel-button');
 
 const emojiContext = emojiCanvas.getContext('2d');
 const imageContext = imageCanvas.getContext('2d');
+
+const tasks = [];
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -28,15 +31,29 @@ form.addEventListener('submit', function(e) {
   }
 });
 
+cancelButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  cancel();
+});
+
 function run(emojiSize, subdivisions, file) {
+  cancel();
   outputContainer.innerHTML = '';
   progressContainer.innerHTML = '';
 
   Promise.all([
     processEmoji(emojiList, emojiSize, subdivisions),
     processImage(file, emojiSize, subdivisions),
-  ]).then(matchEmoji);
+  ])
+  .then(matchEmoji)
+  .catch(err => console.log(err));
 };
+
+function cancel() {
+  while (tasks.length > 0) {
+    tasks.pop().cancel();
+  }
+}
 
 function processEmoji(emojiArray, emojiSize, subdivisions) {
   emojiCanvas.setAttribute('height', emojiSize);
@@ -44,7 +61,7 @@ function processEmoji(emojiArray, emojiSize, subdivisions) {
   emojiContext.font = emojiSize + 'px/1 "Apple Color Emoji"';
   emojiContext.textAlign = 'center';
 
-  return new IterativeTask('process-emoji', emojiArray, function(emoji) {
+  const task = new IterativeTask('process-emoji', emojiArray, function(emoji) {
     emojiContext.clearRect(0, 0, emojiSize, emojiSize);
     emojiContext.fillText(emoji, emojiSize / 2, emojiSize - (emojiSize * 0.05));
     const colors = sampleCanvasSection(emojiContext, 0, 0, emojiSize, subdivisions);
@@ -53,6 +70,9 @@ function processEmoji(emojiArray, emojiSize, subdivisions) {
       colors
     };
   });
+
+  tasks.push(task);
+  return task.promise;
 };
 
 function processImage(file, emojiSize, subdivisions) {
@@ -61,7 +81,8 @@ function processImage(file, emojiSize, subdivisions) {
     imageCanvas.width = width;
     imageCanvas.height = height;
     imageContext.drawImage(image, 0, 0, width, height);
-    return new IterativeTask('process-image', buildImageSections(image, emojiSize), function({ x, y, size }) {
+
+    const task = new IterativeTask('process-image', buildImageSections(image, emojiSize), function({ x, y, size }) {
       const colors = sampleCanvasSection(imageContext, x, y, size, subdivisions);
       const cacheKey = colors.map(c => c.css).join('');
       return {
@@ -71,13 +92,16 @@ function processImage(file, emojiSize, subdivisions) {
         cacheKey,
       };
     });
+
+    tasks.push(task);
+    return task.promise;
   });
 }
 
 function matchEmoji([emojiSamples, imageSamples]) {
   let cache = {};
 
-  return new IterativeTask('match-emoji', imageSamples, function(section) {
+  const task = new IterativeTask('match-emoji', imageSamples, function(section) {
     let bestFitEmoji = null;
     let lowestDistance = Infinity;
 
@@ -104,4 +128,7 @@ function matchEmoji([emojiSamples, imageSamples]) {
     span.append(document.createTextNode(bestFitEmoji.string));
     outputContainer.append(span);
   });
+
+  tasks.push(task);
+  return task.promise;
 }
--- src/iterative-task.js
+++ src/iterative-task.js
@@ -1,22 +1,36 @@
 import ProgressBar from './progress-bar';
 
 export default class IterativeTask {
-  constructor(name, items, task, delay) {
+  // name
+  // promise
+  // interval
+  // reject
+  // cancel()
 
-    return new Promise(function(resolve, reject) {
+  constructor(name, items, task, delay) {
+    this.name = name;
+
+    this.promise = new Promise((resolve, reject) => {
+      this.reject = reject;
+
       const progressBar = new ProgressBar(name, 0, items.length);
       const results = [];
       let index = 0;
 
-      const interval = window.setInterval(function() {
+      this.interval = window.setInterval(() => {
         results.push(task(items[index]));
         index += 1;
         progressBar.update(index);
         if (index >= items.length) {
-          window.clearInterval(interval);
+          window.clearInterval(this.interval);
           resolve(results);
         }
       }, delay || 0);
     });
+  }
+
+  cancel() {
+    window.clearInterval(this.interval);
+    this.reject(`Task was cancelled: "${this.name}"`);
   }
 }
