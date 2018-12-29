7607833a8410b9a494e0892e91a1fb194bfef1e0
James Childress <jchildress@cvent.com>
2018-12-28 19:30:17 -0800

    cached lab conversions for each Color instance

--- src/color.js
+++ src/color.js
@@ -1,43 +1,63 @@
+import { rgba_to_lab } from 'color-diff/lib/convert';
+import { ciede2000 } from 'color-diff/lib/diff';
+
 const ALPHA_THRESHOLD = 100;
-const CHANNELS = ['r', 'g', 'b', 'a'];
+const CHANNELS = ['R', 'G', 'B', 'A'];
+
+const privateRGBA = Symbol('rgba');
+const privateLAB = Symbol('lab');
+const privateCSS = Symbol('css');
+
+function cssString(rgba) {
+  let values = CHANNELS.map(function(prop) {
+    return Math.round(rgba[prop]);
+  });
+  values[3] = Math.round((values[3] / 255) * 100) / 100;
+  return 'rgba(' + values.join(',') + ')';
+}
 
 export default class Color {
-  constructor(r, g, b, a) {
-    this.r = r || 0;
-    this.g = g || 0;
-    this.b = b || 0;
-    this.a = a || 0;
+
+  constructor(rgba) {
+    this[privateRGBA] = rgba;
+    this[privateLAB] = rgba_to_lab(rgba);
+    this[privateCSS] = cssString(rgba);
   }
 
+  get rgba() { return this[privateRGBA]; }
+  get lab() { return this[privateLAB]; }
+  get css() { return this[privateCSS]; }
+
+  distance(color) {
+    return ciede2000(this.lab, color.lab);
+  }
+
+
   static averageFromImageData({ data }) {
-    const averageColor = new Color();
+    const averageRGBA = {};
     let includedPixelCount = 0;
 
+    CHANNELS.forEach(function(prop) {
+      averageRGBA[prop] = 0;
+    });
+
     for (let i = 0; i < data.length; i += CHANNELS.length) {
-      const pixelColor = {};
+      const pixelRGBA = {};
       CHANNELS.forEach(function(prop, propIndex) {
         const n = data[i + propIndex];
-        pixelColor[prop] = n * n;
+        pixelRGBA[prop] = n * n;
       });
-      if (pixelColor.a > ALPHA_THRESHOLD) {
+      if (pixelRGBA.A > ALPHA_THRESHOLD) {
         includedPixelCount += 1;
         CHANNELS.forEach(function(prop) {
-          averageColor[prop] += pixelColor[prop];
+          averageRGBA[prop] += pixelRGBA[prop];
         });
       }
     }
     CHANNELS.forEach(function(prop) {
-      averageColor[prop] = Math.sqrt(averageColor[prop] / includedPixelCount);
+      averageRGBA[prop] = Math.sqrt(averageRGBA[prop] / includedPixelCount);
     });
 
-    return averageColor;
+    return new Color(averageRGBA);
   }
-
-  get cssString() {
-    let values = CHANNELS.map(function(prop) {
-      return Math.floor(this[prop]);
-    });
-    values[3] = values[3] / 255;
-    return 'rgba(' + values.join(',') + ')';
-  };
 };
